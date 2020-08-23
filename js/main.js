8d63ab6201baa7b83f24d9769e9d89de3953e88c
shubham gupta <shubham.gupta2@aspiringminds.in>
2020-08-24 02:44:25 +0530

    Updates

--- js/main.js
+++ js/main.js
@@ -197,7 +197,7 @@
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
-    autoplay: false,
+    autoplay: true,
     dots: true,
     loop: true,
     items: 1
